b265cbfa171a2967764a6a8a775913c3855c6067
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-04-28 17:05:57 -0500

    Add style for selected category on CategoryIcon.tsx

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,6 +1,8 @@
+"use client"
 import { Category } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 
 type CategoryIconProps = {
@@ -8,8 +10,9 @@ type CategoryIconProps = {
 }
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+    const params = useParams<{category: string}>()
     return (
-        <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+        <div className={`${params.category === category.slug ? "bg-amber-600 text-white" : ""} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
 
             <div className="w-16 h-16 relative">
                 <Image

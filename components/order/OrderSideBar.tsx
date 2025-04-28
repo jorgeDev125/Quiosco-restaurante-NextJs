acac091997d21f68547199aa6d87bcfcd1960bc6
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-04-28 11:50:28 -0500

    Add CategoryIcon.tsx, Render categories data in OrdrSideBar.tsx

--- components/order/OrderSideBar.tsx
+++ components/order/OrderSideBar.tsx
@@ -1,7 +1,25 @@
+import { prisma } from "@/src/lib/prisma"
+import CategoryIcon from "../ui/CategoryIcon"
+
+async function getCategories() {
+  return await prisma.category.findMany()
+}
+
+export default async function OrderSideBar() {
+
+  const categories = await getCategories()
+
+  console.log(categories)
 
 
-export default function OrderSideBar() {
+
   return (
-    <aside className="md:w-72 md:h-screen bg-white">OrderSideBar</aside>
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10">
+        {categories.map(category => 
+          <CategoryIcon key={category.id} category={category}/>
+        )}
+      </nav>
+    </aside>
   )
 }

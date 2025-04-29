c24bf6ea943fe52c10891cb63eda014b54f321ed
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-04-28 19:47:41 -0500

    Add ProductDetails.tsx component and Render chosen products on the OrderSummary

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,9 +1,25 @@
+"use client"
+import { useStore } from "@/src/store"
+import ProductDetails from "./ProductDetails"
 
 
 export default function OrderSummary() {
+
+  const order = useStore((state) => state.order)
+
+
   return (
-    <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 p-5">
-        <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+    <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
+      <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+
+      {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (
+        <div className="mt-5">
+          {order.map(item=> (
+            <ProductDetails key={item.id} item={item} />
+              
+          ))}
+        </div>
+      )}
     </aside>
 
   )
--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -12,8 +12,10 @@ type CategoryIconProps = {
 export default function CategoryIcon({ category }: CategoryIconProps) {
     const params = useParams<{category: string}>()
     return (
-        <div className={`${params.category === category.slug ? "bg-amber-600 text-white" : ""} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
-
+        <Link 
+            href={`/order/${category.slug}`} 
+            className={`${params.category === category.slug ? "bg-amber-600 text-white" : ""} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+        >
             <div className="w-16 h-16 relative">
                 <Image
                     fill
@@ -21,10 +23,7 @@ export default function CategoryIcon({ category }: CategoryIconProps) {
                     alt="Category Image"
                 />
             </div>
-            <Link 
-                className="text-xl font-bold"
-                href={`/order/${category.slug}`}
-            >{category.name}</Link>
-        </div>
+            <div className="text-xl font-bold">{category.name}</div>
+        </Link>
     )
 }

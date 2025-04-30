db0c2a75ce43466a36ab8c91928b153e77c595e6
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-04-29 21:51:27 -0500

    Add decreaseQuantity and removeItem function

--- src/store.ts
+++ src/store.ts
@@ -6,6 +6,8 @@ interface Store {
     order: OrderItem[]
     addToOrder: (product: Product) => void
     increaseQuantity: (id: Product["id"]) => void
+    decreaseQuantity: (id: Product["id"]) => void
+    removeItem: (id: Product["id"]) => void
 }
 
 export const useStore = create<Store>((set, get)=> ({
@@ -37,6 +39,21 @@ export const useStore = create<Store>((set, get)=> ({
                 quantity: item.quantity + 1,
                 subtotal: item.price * (item.quantity + 1)
             } : item ) 
+        }))
+    },
+    decreaseQuantity: ( id ) => {
+        const order= get().order.map( item => item.id === id ? {
+                ...item,
+                quantity: item.quantity - 1,
+                subtotal: item.price * (item.quantity - 1)
+            } : item ) 
+        set(()=>({
+            order
+        }))
+    },
+    removeItem: ( id ) => {
+        set(( state )=> ({
+            order: state.order.filter( item => item.id !== id ) 
         }))
     }
 }))

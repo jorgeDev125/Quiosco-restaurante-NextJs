068d007ed28d086c2adbc64f02d592385a6d4b84
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-01 18:25:36 -0500

    "Validate order data in client"

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,6 +3,8 @@ import { useStore } from "@/src/store"
 import ProductDetails from "./ProductDetails"
 import { formatCurrency } from "@/src/utils"
 import { useMemo } from "react"
+import { OrderSchema } from "@/src/schema"
+import { toast } from "react-toastify"
 
 
 export default function OrderSummary() {
@@ -10,12 +12,30 @@ export default function OrderSummary() {
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) ,[order])
 
+  const handleCreateOrder = (formData: FormData) => {
+    const data = {
+      name: formData.get("name")
+    }
+
+    const result = OrderSchema.safeParse(data)
+
+    if (!result.success) {
+      result.error.issues.forEach(issue => {
+        toast.error(issue.message)
+      });
+      return
+    }
+
+  }
+
+
+
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
-      {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (
+      {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio</p> : (
         <div className="mt-5">
           {order.map(item=> (
             <ProductDetails key={item.id} item={item} />
@@ -26,6 +46,25 @@ export default function OrderSummary() {
             Total a Pagar: {" "}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+          <form 
+            className="w-full mt-10 space-y-5"
+            action={handleCreateOrder}  
+          >
+            <input
+              type="text"
+              placeholder="Tu nombre"
+              className="bg-white border border-b-gray-100 p-2 w-full"
+              name="name"
+            />
+            <input 
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center font-bold cursor-pointer"
+              value="Confirmar Pedido"
+            />
+
+          </form>
+
+
         </div>
       )}
     </aside>

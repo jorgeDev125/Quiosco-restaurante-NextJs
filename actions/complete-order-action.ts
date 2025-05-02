6ba7e2b586d854e82ead132c518989f7c3a7a905
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-02 10:28:05 -0500

    Add revalidatePath after complete-order-action

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,5 +1,5 @@
 "use server";
-
+import { revalidatePath } from "next/cache"
 import { prisma } from "@/src/lib/prisma";
 import { OrderIdSchema } from "@/src/schema";
 
@@ -20,6 +20,7 @@ export async function completeOrder(formData: FormData) {
             orderReadyAt: new Date(Date.now()),
           },
         });
+        revalidatePath("/admin/orders")
       } catch (error) {
         console.log(error);
       }

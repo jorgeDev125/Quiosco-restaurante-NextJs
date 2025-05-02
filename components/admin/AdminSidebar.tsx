f274c064f6e00ab66b7e43abeeff0395da82cc0d
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-01 22:26:38 -0500

    Add AdminSideBar.tsx

---
+++ components/admin/AdminSidebar.tsx
@@ -0,0 +1,19 @@
+import Logo from "../ui/Logo"
+
+
+export default function AdminSidebar() {
+
+    return (
+        <>
+            <Logo />
+
+            <div className="space-y-3 ">
+                <p className="mt-10 uppercase font-bold text-sm text-gray-600 text-center">Navegación</p>
+                <nav className="flex flex-col">
+                    
+                </nav>
+            </div>
+        </>
+
+    )
+}

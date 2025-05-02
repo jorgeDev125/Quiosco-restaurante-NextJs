112910cfa64bf07e95c45a02adeb9e740d0e929d
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-02 12:53:57 -0500

    Add number to pagination

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -6,6 +6,11 @@ type ProductsPaginationProps = {
 }
 
 export default function ProductsPagination({page, totalPages}: ProductsPaginationProps) {
+
+  const pages = Array.from({length: totalPages}, (_, i)=> i + 1 )
+  console.log(pages)
+
+
   return (
     <nav className="flex justify-center py-10">
       { page > 1 && (
@@ -14,6 +19,17 @@ export default function ProductsPagination({page, totalPages}: ProductsPaginatio
           href={`/admin/products?page=${page - 1}`}
         >&laquo;</Link>
       )}
+      {
+        pages.map(currentPage => (
+          <Link 
+            key={currentPage}
+            className={`${page === currentPage && "font-black bg-amber-600"} bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
+            href={`/admin/products?page=${currentPage}`}  
+          >
+            {currentPage}
+          </Link>
+        ))
+      }
       { page < totalPages && (
         <Link
           className="bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0"

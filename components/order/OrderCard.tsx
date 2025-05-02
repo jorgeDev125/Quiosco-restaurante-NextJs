bc97f52aca83a88828950dad1125117b541ae855
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-02 00:18:27 -0500

    organize OrderCard.tsx

--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,4 +1,5 @@
 import { OrderWithProducts } from "@/src/types"
+import { formatCurrency } from '../../src/utils/index';
 
 
 type OrderCardProps = {
@@ -12,16 +13,21 @@ export default function OrderCard({ order }: OrderCardProps) {
             className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
         >
             <p className='text-2xl font-medium text-gray-900'>Cliente: {order.name} </p>
-            <p className='text-lg font-medium text-gray-900'>
-                Productos Ordenados: {order.orderProducts.map(product => (
-                    <p key={product.id}>{product.product.name}</p>
-                ))} 
-            </p>
+            <p className='text-lg font-medium text-gray-900'>Productos Ordenados:</p>
             <dl className="mt-6 space-y-4">
-        
+                {order.orderProducts.map(product => (
+                    <div key={product.id} className="flex items-center gap-2 border-t border-gray-200 pt-4">
+                       
+                        <dt className="flex items-center text-sm text-gray-600"> 
+                            <span className="font-black">({product.quantity}) {" "}</span>
+                        </dt>
+                        <dd className="text-sm font-medium text-gray-900">{product.product.name}</dd>
+                    </div>
+
+                ))} 
                 <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-                    <dt className="text-base font-medium text-gray-900">Total a Pagar: {order.total} </dt>
-                    <dd className="text-base font-medium text-gray-900">{}</dd>
+                    <dt className="text-base font-medium text-gray-900">Total a Pagar:</dt>
+                    <dd className="text-base font-medium text-gray-900">{formatCurrency(order.total)}</dd>
                 </div>
             </dl>
 

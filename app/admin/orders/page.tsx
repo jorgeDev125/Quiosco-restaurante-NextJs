439ef28f0cebb133adb281c9df6357c595a5ee70
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-05 22:11:15 -0500

    Add SWR for automatic refreshing

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,55 +1,31 @@
+"use client"
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
-import { revalidatePath } from "next/cache";
+import { OrderWithProducts } from "@/src/types";
+import useSWR from "swr";
 
-export const getPendingOrders = async () => {
-    const orders = await prisma.order.findMany({
-        where: {
-            status: false
-        },
-        include: {
-            orderProducts: {
-                include: {
-                    product: true
-                }
-            }
-        }
+export default  function OrdersPage() {
+    const url = "/admin/orders/api"
+    const fetcher = () => fetch(url).then(res=> res.json()).then(data=>data)
+    const { data, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+        refreshInterval: 60000,
+        revalidateOnFocus: false,
     })
-    return orders
-}
 
-
-export default async function OrdersPage() {
-    const orders = await getPendingOrders()
-    return (
+    if (isLoading) return <p>Cargando...</p>
+    if (data) return (
         <>
             <Heading>Administrar ordenes</Heading>
-
-            <form
-                action={async () => {
-                    "use server"
-                    revalidatePath("/admin/orders")
-                }}
-            >
-                <input
-                    type="submit"
-                    className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
-                    value="Actualizar ordenes"
-                />
-            </form>
-            {orders.length ? (
+            {data.length ? (
                 <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-                    {orders.map(order => (
+                    {data.map(order => (
                         <OrderCard
                             key={order.id}
                             order={order}
                         />
                     ))}
-
                 </div>
             ) : <p className="text-center">No hay ordenes pendientes</p>}
-
         </>
     )
 }
---
+++ app/orders/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import LastOrderItem from "@/components/order/LastOrderItem";
+import Logo from "@/components/ui/Logo";
+import { OrderWithProducts } from "@/src/types";
+import useSWR from "swr";
+
+export default function OrdersPage() {
+    const url = "/orders/api"
+    const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+    const { data, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+        refreshInterval: 60000,
+        revalidateOnFocus: false,
+    })
+
+    if (isLoading) return <p>Cargando...</p>
+    if (data) return (
+        <>
+            <h1 className="text-center mt-20 text-6xl font-black">Ordenes Listas</h1>
+            <Logo />
+                {data.length ? (
+                    <div className="grid grid-cols-1  sm:grid-cols-2 gap-5 max-w-5xl mx-auto mt-10 ">
+                        {data.map(order => (
+                        <LastOrderItem 
+                            key={order.id} 
+                            order={order}
+                        />
+                        ))}
+                    </div>
+                ) : <p className="text-center my-10">No hay ordenes listas</p>}
+        </>
+    )
+}
